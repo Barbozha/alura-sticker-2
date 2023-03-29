5a8224b01fdcab47a5bfe5097226037a5483e231
Barbozha <barbozha@gmail.com>
2023-03-29 12:01:19 -0300

    Limitando para 4 imagens

--- src/application/App.java
+++ src/application/App.java
@@ -31,9 +31,11 @@ public class App {
 
 		// EXIBE OS DADOS ORIUNDO DA API DO IMDB
 		GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
-		for (Map<String, String> filme : listaDeFilmes) {
+		
+		for(int i=1; i < 5; i++) { 
+			Map<String, String> filme = listaDeFilmes.get(i);
 
-			String urlImagem = filme.get("image");
+			String urlImagem = filme.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
 			String titulo = filme.get("title");
 
 			InputStream inputStream = new URL(urlImagem).openStream();
